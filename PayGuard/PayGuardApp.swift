987586b6ca02f624e40f4647ac7a0c301b0a673f
kajumac07 <kajumac13@gmail.com>
2026-01-08 11:57:37 +0530

    Initial Commit

---
+++ PayGuard/PayGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayGuardApp.swift
+//  PayGuard
+//
+//  Created by Atul Tiwari on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PayGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
